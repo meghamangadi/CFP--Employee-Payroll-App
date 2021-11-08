005c042dababe476bf2cea0a4aa95f9aceb06fe1
megha angadi <meghamangadi@gmail.com>
2021-11-08 20:23:55 +0530

    Added : Welcome to Employee Payroll App

---
+++ src/main/java/com/bridgelabz/employepayrollapp/EmployepayrollappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployepayrollappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployepayrollappApplication.class, args);
+	}
+
+}
