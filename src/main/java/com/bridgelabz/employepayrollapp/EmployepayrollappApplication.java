c03463f69718830e64ea2bcc5fdff8b78987b406
megha angadi <meghamangadi@gmail.com>
2021-11-12 07:01:49 +0530

    Added : @Slf4j Logger is added using Lombok

--- src/main/java/com/bridgelabz/employepayrollapp/EmployepayrollappApplication.java
+++ src/main/java/com/bridgelabz/employepayrollapp/EmployepayrollappApplication.java
@@ -3,11 +3,15 @@ package com.bridgelabz.employepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployepayrollappApplication {
 
 	public static void main(String[] args) {
-		
+
+		log.info("Welcome to the Employee Payroll System");
 		SpringApplication.run(EmployepayrollappApplication.class, args);
 	}
 
--- src/main/java/com/bridgelabz/employepayrollapp/controller/EmployeePayRollController.java
+++ src/main/java/com/bridgelabz/employepayrollapp/controller/EmployeePayRollController.java
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController; 
 import com.bridgelabz.employepayrollapp.model.EmployeePayRoll;
 import com.bridgelabz.employepayrollapp.response.Response;
 import com.bridgelabz.employepayrollapp.service.EmployeeServiceImpl;
 
-@RestController
+import lombok.extern.slf4j.Slf4j;
 
+@RestController
+@Slf4j
 public class EmployeePayRollController {
 
 	@Autowired
@@ -24,30 +26,35 @@ public class EmployeePayRollController {
 	 
 	@PostMapping("/saveemployee")
 	public ResponseEntity<Response> saveEmployee(@RequestBody EmployeePayRoll contactInfo) {
+		log.info("save method is called ");
 		Response response = employeeServiceImpl.saveEmployeeDetails(contactInfo);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@GetMapping("/getemployeeId")
 	public ResponseEntity<Response> getContactByID(@RequestParam Long Id) {
+		log.info("Get the employee details by Id  {}", Id);
 		Response response = employeeServiceImpl.getEmployeeByID(Id);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@GetMapping("/getAllemployees")
 	public ResponseEntity<Response> getAllContact() {
+		log.info("Get all employee details ");
 		Response response = employeeServiceImpl.getAllEmployeeDetails();
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@PutMapping("/updateemployeeId")
 	public ResponseEntity<Response> updateEmployeetByID(@RequestParam Long Id, @RequestBody EmployeePayRoll emp) {
+		log.info("update employee details ");
 		Response response = employeeServiceImpl.updateEmployeeByID(Id, emp);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/deleteemployeeId")
 	public ResponseEntity<Response> deleteEmployeeByID(@RequestParam Long Id) {
+		log.info("Delet employee details ");
 		Response response = employeeServiceImpl.deleteEmployeeByID(Id);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
